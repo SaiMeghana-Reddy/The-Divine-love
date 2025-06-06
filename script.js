8ca0970d8bdd5fba66ee68b42d8b60367838a1e4
SaiMeghana-Reddy <saimeghanareddy1075@gmail.com>
2025-06-06 20:29:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById('readbutton');
+  button.addEventListener('click', function () {
+    alert("🎉 You've successfully read the blog!");
+  });
+});
+
